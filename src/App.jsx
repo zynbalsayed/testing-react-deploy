208be415d70c932171ac4f1ce66b79fcb1dff422
zynbalsayed <zainabalsayed84@gmail.com>
2026-02-14 16:15:52 +0300

    added title

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Zainab </h1>
+      <h1>Zainab Sayed </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
